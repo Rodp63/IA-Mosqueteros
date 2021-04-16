a5297d268fc90247868982df5e9f5ecd93d77c39
Rodp63 <joaquin.palma@ucsp.edu.pe>
2021-04-15 19:46:39 -0500

    Reorganizing branches

--- AI/shaders/blue1.fs
+++
@@ -1,7 +0,0 @@
-#version 330 core
-out vec4 FragColor;
-
-void main()
-{
-  FragColor = vec4(0.121f, 0.262f, 0.588f, 1.0f);
-}
--- AI/shaders/marine1.fs
+++
@@ -1,7 +0,0 @@
-#version 330 core
-out vec4 FragColor;
-
-void main()
-{
-  FragColor = vec4(0.17255f, 0.1686f, 0.42745f, 1.0f);
-}
